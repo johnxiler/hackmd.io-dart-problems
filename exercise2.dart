951b2cd6b7e3aaf2a4529093c8e3fc4dc81d5f9c
johnxiler <johnxiler@gmail.com>
2025-02-12 05:48:13 +0800

    push exercise1,2,3

---
+++ exercise2.dart
@@ -0,0 +1,16 @@
+/*
+Ask the user for a number. Depending on whether the number is even or odd, print out an appropriate message to the user.
+*/
+
+import 'dart:io';
+
+main(){
+    stdout.write("Enter a number: ");
+    int number = int.parse(stdin.readLineSync().toString());
+    if(number % 2 == 0){
+        print("The number is even.");
+    } else {
+        print("The number is odd.");
+    }
+    
+}
