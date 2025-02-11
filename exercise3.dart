c125c5c953a4e073198fdf172f970a1a98c32aa1
johnxiler <johnxiler@gmail.com>
2025-02-12 06:01:04 +0800

    naming convention based on my preferences

--- exercise3.dart
+++ exercise3.dart
@@ -8,8 +8,8 @@ and write a program that prints out all the elements of the list that are less t
 void main(){
     List<int> numeroListo = [1, 1, 2, 3, 5, 8, 13, 21, 34, 55, 89];
     for(int baseNumber = 0; baseNumber < numeroListo.length; baseNumber++){
-        if(numeroListo[i] < 5){
-            print(numeroListo[i]);
+        if(numeroListo[baseNumber] < 5){
+            print(numeroListo[baseNumber]);
         }
     }
 }
